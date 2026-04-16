e6ed9cfc09dcb05e8621130e514aa2c3a8b218d1
root <root@intelligent-bird-hides-fin-01.datacrunch.io>
2026-04-16 02:17:01 +0000

    add kernel

---
+++ triton_work/vector_addition.py
@@ -0,0 +1,73 @@
+# %%
+import torch
+
+import triton
+import triton.language as tl
+
+DEVICE = 'cuda'
+# %%
+@triton.jit
+def add_kernel(
+    x_ptr, # pointer to the input vector x
+    y_ptr, # pointer to the input vector y
+    output_ptr, # pointer to the output vector
+    n_elements, # so that we will know where to stop
+    BLOCK_SIZE: tl.constexpr, # the size of the block
+):
+    # identify which program we are running
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    out = x + y
+    tl.store(output_ptr + offsets, out, mask=mask)
+# %%
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x)
+    assert x.is_cuda and y.is_cuda and output.is_cuda
+    n_elements = output.numel()
+    block_size = 1024
+    num_blocks = triton.cdiv(n_elements, block_size)
+    add_kernel[(num_blocks,)](x,y,output,n_elements,block_size)
+    return output
+
+# %%
+torch.manual_seed(0)
+size = 98432
+x = torch.rand(size, device=DEVICE)
+y = torch.rand(size, device=DEVICE)
+output_torch = x + y
+output_triton = add(x, y)
+print(output_torch)
+print(output_triton)
+print(f'The maximum difference between torch and triton is '
+      f'{torch.max(torch.abs(output_torch - output_triton))}')
+# %%
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['size'],  # Argument names to use as an x-axis for the plot.
+        x_vals=[2**i for i in range(12, 28, 1)],  # Different possible values for `x_name`.
+        x_log=True,  # x axis is logarithmic.
+        line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
+        line_vals=['triton', 'torch'],  # Possible values for `line_arg`.
+        line_names=['Triton', 'Torch'],  # Label name for the lines.
+        styles=[('blue', '-'), ('green', '-')],  # Line styles.
+        ylabel='GB/s',  # Label name for the y-axis.
+        plot_name='vector-add-performance',  # Name for the plot. Used also as a file name for saving the plot.
+        args={},  # Values for function arguments not in `x_names` and `y_name`.
+    ))
+def benchmark(size, provider):
+    x = torch.rand(size, device=DEVICE, dtype=torch.float32)
+    y = torch.rand(size, device=DEVICE, dtype=torch.float32)
+    quantiles = [0.5, 0.2, 0.8]
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x + y, quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
+    gbps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+# %%
+benchmark.run(print_data=True, show_plots=True)
+# %%
